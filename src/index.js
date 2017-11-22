bf62156fcf9ca2cd4a601ac6b66699eb74bb9d93
zjcwill <zjcwill@foxmail.com>
2017-11-22 23:06:40 +0800

    添加官网demo路由

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Router from './routes/main';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const Index = (props) => (
+//     <div>
+//         <header>header</header>
+//         <div className="main">
+//             {props.children}
+//         </div>
+//         <footer>footer</footer>
+//     </div>
+// )
+
+
+// const Hello = () => (
+//     <div>
+//         <h1>我是首页</h1>
+//         <Link to="/demo">demo</Link>
+//     </div>
+// )
+
+ReactDOM.render(<Router />, document.getElementById('root'));
 registerServiceWorker();
