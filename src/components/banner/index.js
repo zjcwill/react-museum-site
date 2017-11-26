d0ce7b8216f850d6343718c787dd8f8964258542
nyle.zhang <zjcwill@foxmail.com>
2017-11-26 20:27:57 +0800

    抽出轮播图组件

---
+++ src/components/banner/index.js
@@ -0,0 +1,21 @@
+/**
+ * 轮播图组件
+ */
+import React from 'react';
+import { Carousel } from 'antd';
+
+const BannerComponent = (props) => {
+    return (
+        <div>
+            <Carousel autoplay>
+                <div><img alt="banner" src={props.imgUrl[0]}></img></div>
+                <div><img alt="banner" src={props.imgUrl[1]}></img></div>
+                <div><img alt="banner" src={props.imgUrl[2]}></img></div>
+                <div><img alt="banner" src={props.imgUrl[3]}></img></div>
+            </Carousel>
+        </div>
+
+    )
+}
+
+export default BannerComponent;
--- src/routes/IndexPage/index.js
+++ src/routes/IndexPage/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import { actions, connect } from 'mirrorx';
 import { Carousel } from 'antd';
+import Banner from '../../components/banner';
 import './container';
 import './index.css';
 
 const IndexPage = connect(state => { return { data: state.IndexPage } })(props => (
     <div>
-        <Carousel autoplay>
-            <div><img alt="banner" src={props.data.bannerURL[0]}></img></div>
-            <div><img alt="banner" src={props.data.bannerURL[0]}></img></div>
-            <div><img alt="banner" src={props.data.bannerURL[0]}></img></div>
-            <div><img alt="banner" src={props.data.bannerURL[0]}></img></div>
-        </Carousel>
+        <Banner imgUrl={props.data.bannerURL}/>
         <div style={{ height: "100px" }}>
             <h3>公告栏</h3>
         </div>
