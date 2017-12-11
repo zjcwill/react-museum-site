c54529fd3b5eef2004ad1010553acf68bf315855
nyle.zhang <nyle.zhang@vipshop.com>
2017-12-11 16:43:25 +0800

    增加公告栏

--- src/components/informationBoard/index.js
+++ src/components/informationBoard/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Card, Row, Col } from 'antd';
 import './index.css';
 
 const InformationBoard = () => (
-    <div className="informationboard-container">
-        <h3>公告栏</h3>
-    </div>
+    <Row className="informationboard-container">
+        <Col span={6}>
+                <Card className="informationboard-card">
+                    hello world
+                </Card>
+        </Col>
+        <Col span={6}>
+                <Card className="informationboard-card">
+                    hello world
+                </Card>
+        </Col>
+        <Col span={6}>
+                <Card className="informationboard-card">
+                    hello world
+                </Card>
+        </Col>
+        <Col span={6}>
+                <Card className="informationboard-card">
+                    hello world
+                </Card>
+        </Col>
+    </Row>
 )
 
 export default InformationBoard;
