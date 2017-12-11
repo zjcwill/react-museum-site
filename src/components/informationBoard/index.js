799ca69a2279382b82b3a2c617f3e098d66c638a
nyle.zhang <nyle.zhang@vipshop.com>
2017-12-11 15:52:15 +0800

    拆分公告栏模块

---
+++ src/components/informationBoard/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.css';
+
+const InformationBoard = () => (
+    <div className="informationboard-container">
+        <h3>公告栏</h3>
+    </div>
+)
+
+export default InformationBoard;
--- src/routes/IndexPage/index.js
+++ src/routes/IndexPage/index.js
@@ -2,15 +2,14 @@ import React from 'react';
 import { actions, connect } from 'mirrorx';
 import { Carousel } from 'antd';
 import Banner from '../../components/banner';
+import InformationBoard from '../../components/informationBoard';
 import './container';
 import './index.css';
 
 const IndexPage = connect(state => { return { data: state.IndexPage } })(props => (
     <div>
         <Banner imgUrl={props.data.bannerURL}/>
-        <div style={{ height: "100px" }}>
-            <h3>公告栏</h3>
-        </div>
+        <InformationBoard/>
         <div style={{ height: "300px", backgroundColor: "green" }}>
             <h3>特色模块展示</h3>
         </div>
