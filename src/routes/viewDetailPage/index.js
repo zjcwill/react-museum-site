dcfd4019b3add4b97c75bae4919ca752d5114dd8
nyle.zhang <nyle.zhang@vipshop.com>
2017-12-15 16:36:17 +0800

    添加全景图详情页

---
+++ src/routes/viewDetailPage/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ViewDetailPage extends React.Component{
+    render(){
+        return (
+            <div>
+                详情页
+            </div>
+        )
+    }
+}
+
+export default ViewDetailPage;
